7ce56fc564bfbd3d5b4d892d68300e644eaaa8e8
The-alpha-male <ronnymunene@outlook.com>
2024-05-12 00:36:13 +0300

    script that creates a stored procedure that adds a new correlation for a student

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,16 @@
+-- script that creates a stored procedure
+-- AddBonus that adds a new correction for a student
+DELIMETER $$;
+CREATE PROCEDURE AddBonus(
+    IN student_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name) THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id,(SELECT id FROM projects WHERE name = project_name), score);
+END $$;
+DELIMETER ;
