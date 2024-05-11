8822982848ddc1c350b87d9bce9d348b28cab8c2
The-alpha-male <ronnymunene@outlook.com>
2024-05-11 21:58:48 +0300

    created users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
---Create table users
+-- script that creates a table users with following fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
     PRIMARY KEY (id)
 );
