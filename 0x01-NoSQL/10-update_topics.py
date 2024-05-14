22ec2aafaa22e817699d27b52c2401731bc54e96
The-alpha-male <ronnymunene@outlook.com>
2024-05-14 15:26:01 +0300

    Changed all topics of a school document based on the name

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,25 @@
+# #!/usr/bin/env python3
+
+# def update_topics(mongo_collection, name, topics):
+#     """Change all topics of the school doc based on the name arg
+#     args:
+#     mongo_collection: collection to update
+#     name: name of the school to update
+#     @topics: list to update
+#     """
+#     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+
 #!/usr/bin/env python3
+"""
+a Python function that changes all topics of a school document based on the name
+"""
+
 
 def update_topics(mongo_collection, name, topics):
-    """Change all topics of the school doc based on the name arg
-    args:
-    mongo_collection: collection to update
-    name: name of the school to update
-    @topics: list to update
     """
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    changes all topics of a school document based on the name
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
