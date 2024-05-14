a77b2ab068cb50397565f62599cb7fa410ee129d
The-alpha-male <ronnymunene@outlook.com>
2024-05-14 16:05:43 +0300

    Resolved Pycodestyle issues

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,17 +1,7 @@
-# #!/usr/bin/env python3
-
-# def update_topics(mongo_collection, name, topics):
-#     """Change all topics of the school doc based on the name arg
-#     args:
-#     mongo_collection: collection to update
-#     name: name of the school to update
-#     @topics: list to update
-#     """
-#     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
-
 #!/usr/bin/env python3
 """
-a Python function that changes all topics of a school document based on the name
+a Python function that changes all topics
+of a school document based on the name
 """
 
 
