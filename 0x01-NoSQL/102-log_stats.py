fb87a144539e33d5637223cd010ca74ee78981a3
The-alpha-male <ronnymunene@outlook.com>
2024-05-14 16:26:39 +0300

    adding the top 10 of the most present IPs in the collection nginx of the database logs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
-
-"""
-Improve 12-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs
+"""15 Log stats - new version
+add top 10 of the most present IPs in the collection
+nginx of the database logs
 """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017')
-    collection = client.logs.nginx
+    nginx_collection = MongoClient().logs.nginx
 
-    print("{} logs".format(collection.count_documents({})))
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        print("\tmethod {}: {}".format(method, collection.count_documents({"method": method})))
-    print("{} status check".format(collection.count_documents({"path": "/status"})))
+    print(f"{nginx_collection.count_documents({})} logs")
 
-    print("IPs:")
-    ips = collection.aggregate([
+    print(f"Methods:")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    method_get_count = nginx_collection.count_documents({"method": "GET",
+                                                         "path": "/status"})
+    print(f"{method_get_count} status check")
+
+    ip_top_10 = nginx_collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ])
-    for ip in ips:
-        print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
+
+    print("IPs:")
+    for ip in ip_top_10:
+        print(f"\t{ip['_id']}: {ip['count']}")
