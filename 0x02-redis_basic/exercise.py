1666bc00b74fe7a0388fa792e1172974f9db25a4
The-alpha-male <ronnymunene@outlook.com>
2024-05-15 17:40:58 +0300

    creted a get method that takes a key arg and an optional Cllable arg named fn

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(name=key, value=data)
         return key
+
+     def get(self, key: str, fn: Optional[Callable] =
+            None) -> Union[str, bytes, int, float]:
+        """Get from redis
+        """
+        value = self._redis.get(name=key)
+        if value is None:
+            return value
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Get from redis
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Get from redis
+        """
+        value = self.get(key)
+        return int(value) if value else None
